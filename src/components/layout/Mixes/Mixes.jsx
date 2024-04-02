b926bd818c266ad20d25f0b5784899a161779902
hasanarpat <black_the_dragon@hotmail.com>
2024-04-02 18:08:36 +0300

    complete mixes design

--- src/components/layout/Mixes/Mixes.jsx
+++ src/components/layout/Mixes/Mixes.jsx
@@ -1,19 +1,154 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { cn } from '@/lib/utils';
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 const Mixes = () => {
-  const rectRef = useRef(null);
+  const [width, setWidth] = useState(0);
+  const ref = useRef(null);
   useEffect(() => {
-    const width = rectRef.current.offsetWidth;
-  }, [rectRef.current]);
+    if (!ref.current) return;
+    const resizeObserver = new ResizeObserver(() => {
+      if (ref.current) {
+        const { current } = ref;
+        const boundingRect = current.getBoundingClientRect();
+        const { width } = boundingRect;
+        setWidth(Math.round(width));
+      }
+    });
+    resizeObserver.observe(ref.current);
+    return () => resizeObserver.disconnect();
+  }, [ref]);
 
   return (
-    <div className='w-full flex items-start gap-2' ref={rectRef}>
-      <div className='flex-1 min-w-md'>
-        <div className='h-12 w-full bg-red-500 mt-1 mr-1'></div>
+    <div
+      className={cn(
+        'w-full grid gap-2 p-4 pt-0',
+        width < 800 ? 'grid-cols-2' : ''
+      )}
+      ref={ref}
+    >
+      <div className='grid w-full gap-2'>
+        <div
+          className={cn(
+            'w-full grid gap-1',
+            width < 800 ? 'grid-cols-1 h-60' : 'grid-cols-4 h-12'
+          )}
+        >
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 flex items-center gap-2 hover:bg-zinc-400/40'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>{' '}
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>{' '}
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>
+        </div>
       </div>
-      <div className='flex-1 min-w-md'>
-        <div className='h-12 w-full bg-sky-500 mt-1 mr-1'></div>
+      <div className='grid w-full gap-2'>
+        <div
+          className={cn(
+            'w-full grid gap-1',
+            width < 800 ? 'grid-cols-1 h-60' : 'grid-cols-4 h-12'
+          )}
+        >
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>{' '}
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>{' '}
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>{' '}
+          <div className='w-full h-full bg-zinc-700/20 bg-opacity-45 hover:bg-zinc-400/40 flex items-center gap-2'>
+            <div className='rounded-sm relative w-[36px] h-[36px]'>
+              <Image
+                alt='album'
+                src='https://images.unsplash.com/photo-1526656001029-20a71b17f7ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+                fill
+                className='object-cover rounded-sm'
+              />
+            </div>
+            <span className='text-sm text-primary-foreground/10'>
+              Hüzünlü Mix
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
